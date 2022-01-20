6c4649c4db9127ef90cd8ea767c52dbf5d5fd3f2
Ankit Nagar <dhakedbhai786@gmail.com>
2022-01-21 03:04:47 +0530

    myProject

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const socket = require("socket.io");
 const app = express(); //Initialized and server ready
 
 app.use(express.static("public"));
-//process.env.PORT ||
-let port = 5000;
+//
+let port = process.env.PORT || 5000;
 let server = app.listen(port, () => {
     console.log("Listening to port" + port);
 })
